901e4d3605b9c6b7e694c9108899a6c8c95bb71f
Yannick Rd <yannick.rd@tuta.io>
2025-03-19 16:42:35 +0700

    Add last changes to raycasting calculations

--- src/maths.c/maths1.c
+++ src/maths.c/maths1.c
@@ -6,19 +6,11 @@
 /*   By: yrouzaud <yrouzaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 09:31:20 by yrouzaud          #+#    #+#             */
-/*   Updated: 2025/03/18 12:31:20 by yrouzaud         ###   ########.fr       */
+/*   Updated: 2025/03/19 16:31:20 by yrouzaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// QUESTIONS TO VITA:
-// - float or double or don't mind?
-// - is the current structure(s) the final one, so that I can add var to it?
-// - is there a "mother" struct to be passed to every function? If not, 
-// - is the last version on github? If not, what are you planning to add to it?
-// - working with branches? Should we? How does it works? Any tips/warnings?
-// - about 2D and 3D calculations...?
-
-// 640 * 4 * 400 * 4 is the final
+// 640 * 4 * 400 * 4 seems to be the good resolution
 // pixels texture 124
 
 #include "cub3d.h"
@@ -68,8 +60,8 @@ player_moving_speed = 10;
 
 unit_size = 64;
 
-projection_plane_height = 400;
-projection_plane_width = 640;
+projection_plane_height = 400 * 4;
+projection_plane_width = 640 * 4;
 
 angle_between_rays_rad = player_field_of_view_rad / projection_plane_width;
 
@@ -108,26 +100,49 @@ float	set_alpha_angle(float current_ray_angle)
 	return (alpha_angle_rad);
 }
 
-int	*find_1st_inters_coord(float current_ray_angle)
+int	*find_1st_h_inters_coord(float current_ray_angle)
 {
 	int	first_intersection_coord[2];
 
-	if (....)
-	{
-		if (current_ray_angle < pi)
-			first_intersection_coord[1] = floor(player_position_raw[1] / unit_size) * unit_size - 1; // Shouldn't floor includes all?
-		else
-			first_intersection_coord[1] = floor(player_position_raw[1] / unit_size) * unit_size + 64; // Shouldn't floor includes all?
-		first_intersection_coord[0] = (player_position_raw[1] - first_intersection_coord[1]) / tan(set_alpha_angle(current_ray_angle));
-	}
+	if (current_ray_angle < pi)
+		first_intersection_coord[1] = floor(player_position_raw[1] / unit_size) * unit_size - 1; // Shouldn't floor includes all?
 	else
-	{
-....
-	}
+		first_intersection_coord[1] = floor(player_position_raw[1] / unit_size) * unit_size + 64; // Shouldn't floor includes all?
+	first_intersection_coord[0] = (player_position_raw[1] - first_intersection_coord[1]) / tan(set_alpha_angle(current_ray_angle));
 	return (first_intersection_coord)
 }
 
-int	*find_next_inters_coord(float current_ray_angle, int first_intersection_coord[2], int iteration)
+int	*find_next_h_inters_coord(float current_ray_angle, int first_intersection_coord[2], int iteration)
+{
+	int	next_intersection_coord[2];
+	int	y_increment_value;
+	float	x_increment_value;
+
+	if (current_ray_angle < pi)
+		y_increment_value = unit_size;
+	else
+		y_increment_value = - unit_size;
+	x_increment_value = unit_size / tan(set_alpha_angle(current_ray_angle));
+	next_intersection_coord[0] = (first_intersection_coord[0] + (x_increment_value * iteration)) / unit_size;
+	next_intersection_coord[1] = (first_intersection_coord[1] + (y_increment_value * unit_size)) / unit_size
+	return (next_intersection_coord);
+}
+
+// TO BE REMADE FOR VERTICAL (COPIED FROM HORIZONTAL):
+int	*find_1st_v_inters_coord(float current_ray_angle)
+{
+	int	first_intersection_coord[2];
+
+	if (current_ray_angle < pi)
+		first_intersection_coord[1] = floor(player_position_raw[1] / unit_size) * unit_size - 1; // Shouldn't floor includes all?
+	else
+		first_intersection_coord[1] = floor(player_position_raw[1] / unit_size) * unit_size + 64; // Shouldn't floor includes all?
+	first_intersection_coord[0] = (player_position_raw[1] - first_intersection_coord[1]) / tan(set_alpha_angle(current_ray_angle));
+	return (first_intersection_coord)
+}
+
+// TO BE REMADE FOR VERTICAL (COPIED FROM HORIZONTAL):
+int	*find_next_v_inters_coord(float current_ray_angle, int first_intersection_coord[2], int iteration)
 {
 	int	next_intersection_coord[2];
 	int	y_increment_value;
@@ -160,7 +175,7 @@ float	calc_wall_distance(float current_ray_angle, int intersection_x)
 {
 	float	distance_to_wall;
 
-	distance_horiz_wall = abs(player_position_raw[0] - intersection_x) / cos(set_alpha_angle(current_ray_angle));
+	distance_to_wall = abs(player_position_raw[0] - intersection_x) / cos(set_alpha_angle(current_ray_angle));
 	return(distance_to_wall);
 }
 
@@ -168,26 +183,36 @@ float	first_horizont_wall_dist(float current_ray_angle)
 {
 	int	first_intersection_coord[2];
 	int	next_intersection_coord[2];
-	float	dist_to_1st_horizont_wall;
-	int	iteration_before_wall;
+	int	iterations_before_wall;
 
-	iteration_before_wall = 0;
-	first_intersection_coord = find_1st_inters_coord(current_ray_angle); // will not work like this, need to use pointer instead, but the idea is the same
+	iterations_before_wall = 1;
+	first_intersection_coord = find_1st_h_inters_coord(current_ray_angle); // will not work like this, need to use pointer instead, but the idea is the same
 	if (check_if_wall_at(first_intersection_coord) == 1)
-		return (calc_wall_distance(current_ray_angle, first_horizont_wall_dist[0]));
+		return (calc_wall_distance(current_ray_angle, first_intersection_coord[0]));
 	while (check_if_wall_at(next_intersection_coord) == 0)
 	{
-		next_intersection_coord = find_next_inters_coord(current_ray_angle, iteration_before_wall);  // will not work like this, need to use pointer instead, but the idea is the same
-		iteration_before_wall++;
+		next_intersection_coord = find_next_h_inters_coord(current_ray_angle, iterations_before_wall);  // will not work like this, need to use pointer instead, but the idea is the same
+		iterations_before_wall++;
 	}
 	return(calc_wall_distance(current_ray_angle, next_intersection_coord[0]));
 }
 
 float	first_vertical_wall_dist()
 {
-	float	dist_to_1st_vertical_wall;
+	int	first_intersection_coord[2];
+	int	next_intersection_coord[2];
+	int	iterations_before_wall;
 
-	.......
+	iterations_before_wall = 1;
+	first_intersection_coord = find_1st_v_inters_coord(current_ray_angle); // will not work like this, need to use pointer instead, but the idea is the same
+	if (check_if_wall_at(first_intersection_coord) == 1)
+		return (calc_wall_distance(current_ray_angle, first_intersection_coord[0]));
+	while (check_if_wall_at(next_intersection_coord) == 0)
+	{
+		next_intersection_coord = find_next_v_inters_coord(current_ray_angle, iterations_before_wall); // will not work like this, need to use pointer instead, but the idea is the same
+		iterations_before_wall++;
+	}
+	return(calc_wall_distance(current_ray_angle, next_intersection_coord[0]));
 }
 
 float	closest_wall_distance(float current_ray_angle)
@@ -198,6 +223,14 @@ float	closest_wall_distance(float current_ray_angle)
 	dist_to_1st_horizont_wall = first_horizont_wall_dist(current_ray_angle, .....);
 	dist_to_1st_vertical_wall = first_vertical_wall_dist(, current_ray_angle.....);
 	.........
+	.........
+}
+
+float	correct_fishbowl_effect()
+{
+	........
+	........
+	........
 }
 
 void	iterate_casted_rays()
